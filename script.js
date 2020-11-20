fd8b4f72266891d63f5148087e87a31f703a3452
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2020-11-20 20:15:48 +0100

    added video change script

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ var video = document.getElementById('videoMedia');
 
 document.querySelector('video').playbackRate=0.60;
 document.querySelector('video').play();
+
+
+var video = document.getElementById('videoMedia');
+var source = document.createElement('source');
+
+source.setAttribute('src', 'content/japan.mp4');
+
+video.appendChild(source);
+video.play();
+
+function changeVideo() {
+    source.setAttribute('src', 'content/')
+}
