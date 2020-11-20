ece0ee795a6edde118b1f60237b8090e17b50ba1
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2020-11-20 20:00:00 +0100

    added search bar and working search also added settings icon and working playback speed modifier

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 var video = document.getElementById('videoMedia');
 
-document.querySelector('video').playbackRate=0.75;
+document.querySelector('video').playbackRate=0.60;
 document.querySelector('video').play();
