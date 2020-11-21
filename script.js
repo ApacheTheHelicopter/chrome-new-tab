b75716b4bc7f24f9c094ea9e18e13033a8f7b06f
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2020-11-21 18:48:49 +0100

    everything is broken

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 var video = document.getElementById('videoMedia');
 var source = document.createElement('source');
 
-source.setAttribute('src', 'content/japan.mp4');
+source.setAttribute('src', "content/japan.mp4");
 
 video.appendChild(source);
 video.play();
-
-function changeVideo() {
-    source.setAttribute('src', 'content/sunny_road.mp4');
-}
 
 var video = document.getElementById('videoMedia');
 
 document.querySelector('video').playbackRate=0.60;
 document.querySelector('video').play();
+
+// Time function
+
+function startTime(){
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    m = checkTime(m);
+    document.getElementById("time").innerHTML =
+    h + ":"+ m;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i){
+    if (i < 10) (i = "0" + i); // 0 zu Nummer hinzufügen, wenn sie unter 10 sind
+    return i;
+}
